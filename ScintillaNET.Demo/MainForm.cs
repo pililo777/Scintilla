adf534cffdb615fb20add5a80c3ee004c4ac50b6
Ruben <pililo777@gmail.com>
2018-02-07 01:28:35 +0100

    Lexer propio

--- ScintillaNET.Demo/MainForm.cs
+++ ScintillaNET.Demo/MainForm.cs
@@ -9,9 +9,15 @@ using System.Windows.Forms;
 using System.IO;
 using ScintillaNET;
 using ScintillaNET.Demo.Utils;
+using System.Text.RegularExpressions;
 
 namespace ScintillaNET.Demo {
-	public partial class MainForm : Form {
+
+
+   
+        
+
+    public partial class MainForm : Form {
 		public MainForm() {
 			InitializeComponent();
 		}
@@ -96,7 +102,7 @@ namespace ScintillaNET.Demo {
 			TextArea.StyleClearAll();
 
 			// Configure the CPP (C#) lexer styles
-			TextArea.Styles[Style.Cpp.Identifier].ForeColor = IntToColor(0xD0DAE2);
+			TextArea.Styles[Style.Cpp.Identifier].ForeColor = IntToColor(0xC0EAF2);
 			TextArea.Styles[Style.Cpp.Comment].ForeColor = IntToColor(0xBD758B);
 			TextArea.Styles[Style.Cpp.CommentLine].ForeColor = IntToColor(0x40BF57);
 			TextArea.Styles[Style.Cpp.CommentDoc].ForeColor = IntToColor(0x2FAE35);
@@ -113,10 +119,29 @@ namespace ScintillaNET.Demo {
 			TextArea.Styles[Style.Cpp.CommentDocKeywordError].ForeColor = IntToColor(0xFF0000);
 			TextArea.Styles[Style.Cpp.GlobalClass].ForeColor = IntToColor(0x48A8EE);
 
-			TextArea.Lexer = Lexer.Cpp;
 
-			TextArea.SetKeywords(0, "class extends implements import interface new case do while else if for in switch throw get set function var try catch finally while with default break continue delete return each const namespace package include use is as instanceof typeof author copy default deprecated eventType example exampleText exception haxe inheritDoc internal link mtasc mxmlc param private return see serial serialData serialField since throws usage version langversion playerversion productversion dynamic private public partial static intrinsic internal native override protected AS3 final super this arguments null Infinity NaN undefined true false abstract as base bool break by byte case catch char checked class const continue decimal default delegate do double descending explicit event extern else enum false finally fixed float for foreach from goto group if implicit in int interface internal into is lock long new null namespace object operator out override orderby params private protected public readonly ref return switch struct sbyte sealed short sizeof stackalloc static string select this throw true try typeof uint ulong unchecked unsafe ushort using var virtual volatile void while where yield");
-			TextArea.SetKeywords(1, "void Null ArgumentError arguments Array Boolean Class Date DefinitionError Error EvalError Function int Math Namespace Number Object RangeError ReferenceError RegExp SecurityError String SyntaxError TypeError uint XML XMLList Boolean Byte Char DateTime Decimal Double Int16 Int32 Int64 IntPtr SByte Single UInt16 UInt32 UInt64 UIntPtr Void Path File System Windows Forms ScintillaNET");
+            TextArea.StyleResetDefault();
+            TextArea.Styles[Style.Default].Font = "Consolas";
+            TextArea.Styles[Style.Default].Size = 10;
+            TextArea.Styles[Style.Default].BackColor = IntToColor(0x212121);
+            TextArea.Styles[Style.Default].ForeColor = IntToColor(0xFFFFFF);
+
+
+            TextArea.StyleClearAll();
+
+            TextArea.Styles[CSharpLexer.StyleDefault].ForeColor = Color.Black;
+            TextArea.Styles[CSharpLexer.StyleKeyword].ForeColor = Color.Yellow;
+            TextArea.Styles[CSharpLexer.StyleIdentifier].ForeColor = Color.Teal;
+            TextArea.Styles[CSharpLexer.StyleNumber].ForeColor = Color.Purple;
+            TextArea.Styles[CSharpLexer.StyleString].ForeColor = Color.Red;
+
+            TextArea.Lexer = Lexer.Container;
+ 
+
+            TextArea.StyleNeeded += TextArea_StyleNeeded;
+
+    //         TextArea.SetKeywords(0, "class extends implements import interface new case do while else if for in switch throw get set function var try catch finally while with default break continue delete return each const namespace package include use is as instanceof typeof author copy default deprecated eventType example exampleText exception haxe inheritDoc internal link mtasc mxmlc param private return see serial serialData serialField since throws usage version langversion playerversion productversion dynamic private public partial static intrinsic internal native override protected AS3 final super this arguments null Infinity NaN undefined true false abstract as base bool break by byte case catch char checked class const continue decimal default delegate do double descending explicit event extern else enum false finally fixed float for foreach from goto group if implicit in int interface internal into is lock long new null namespace object operator out override orderby params private protected public readonly ref return switch struct sbyte sealed short sizeof stackalloc static string select this throw true try typeof uint ulong unchecked unsafe ushort using var virtual volatile void while where yield");
+			 //TextArea.SetKeywords(1, "void Null ArgumentError arguments Array Boolean Class Date DefinitionError Error EvalError Function int Math Namespace Number Object RangeError ReferenceError RegExp SecurityError String SyntaxError TypeError uint XML XMLList Boolean Byte Char DateTime Decimal Double Int16 Int32 Int64 IntPtr SByte Single UInt16 UInt32 UInt64 UIntPtr Void Path File System Windows Forms ScintillaNET");
 
 		}
 
@@ -543,10 +568,26 @@ namespace ScintillaNET.Demo {
 			}
 		}
 
-		#endregion
+        #endregion
 
+        private CSharpLexer cSharpLexer = new CSharpLexer("class extends implements import interface new case do while else if for in switch throw get set function var try catch finally while with default break continue delete return each const namespace package include use is as instanceof typeof author copy default deprecated eventType example exampleText exception haxe inheritDoc internal link mtasc mxmlc param private return see serial serialData serialField since throws usage version langversion playerversion productversion dynamic private public partial static intrinsic internal native override protected AS3 final super this arguments null Infinity NaN undefined true false abstract as base bool break by byte case catch char checked class const continue decimal default delegate do double descending explicit event extern else enum false finally fixed float for foreach from goto group if implicit in int interface internal into is lock long new null namespace object operator out override orderby params private protected public readonly ref return switch struct sbyte sealed short sizeof stackalloc static string select this throw true try typeof uint ulong unchecked unsafe ushort using var virtual volatile void while where yield void Null ArgumentError arguments Array Boolean Class Date DefinitionError Error EvalError Function int Math Namespace Number Object RangeError ReferenceError RegExp SecurityError String SyntaxError TypeError uint XML XMLList Boolean Byte Char DateTime Decimal Double Int16 Int32 Int64 IntPtr SByte Single UInt16 UInt32 UInt64 UIntPtr Void Path File System Windows Forms ScintillaNET");
 
+        //private void form_Load(object sender, EventArgs e)
+        //{
 
+        //}
 
-	}
+        private void TextArea_StyleNeeded(object sender, StyleNeededEventArgs e)
+        {
+            var startPos = TextArea.GetEndStyled();
+            var endPos = e.Position;
+
+            cSharpLexer.Style(TextArea, startPos, endPos);
+        }
+    }
 }
+
+
+
+
+
